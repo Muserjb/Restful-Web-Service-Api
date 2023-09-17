e179578d89b49ac7aa4c8cb6e4b552e6f28dc875
Musa usman <musausmanjb@gmail.com>
2023-09-18 00:38:56 +0100

    First Commit on Restful-web-service-Api

---
+++ src/main/java/com/example/websevices/user/UserDaoServices.java
@@ -0,0 +1,53 @@
+package com.example.websevices.user;
+
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoServices {
+    //JPA / Hibernate  > Database
+    private static int userCount = 0;
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(++userCount, "Musa", LocalDate.now().minusYears(30)));
+        users.add(new User(++userCount, "Usman", LocalDate.now().minusYears(31)));
+        users.add(new User(++userCount, "Abubakar", LocalDate.now().minusYears(32)));
+       // users.add(new User(++userCount, "Mahmud", LocalDate.now().minusYears(33)));
+    }
+    public List<User> findAll(){
+        return  users;
+    }
+
+    public User findOneUser(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        User user = users.stream().filter(predicate).findFirst().orElse(null);
+        return user;
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
+
+    public User saveUser(User user) {
+        user.setId(++userCount);
+        users.add(user);
+        return user;
+    }
+
+    // public List<User> findAll()
+
+    // public User saveUser(User user)
+    // public User findOne(int id)
+
+
+
+
+
+}
